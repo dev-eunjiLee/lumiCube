85fd54d0c873c81d2c3c6e73afd42cb474d510c6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-30 00:03:22 +0900

    ADD: (express) hello world 완료

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express();
+const port = 8080;
+
+app.get('/', (_,res) => {
+    res.status(200).send()
+})
+
+app.listen(port, () => console.log(`Running on port ${port}`))
