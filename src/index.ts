ccd2fb4bde17a3c2b6fd510ddfad95d022481fd6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-09-05 22:28:26 +0900

    ADD: express use(미들웨어) 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import App from './App';
-
+import { Request,Response } from 'express';
 const app = new App().application;
+
+app.use('/hi', function (req:Request, res: Response, next: Function){
+  console.log('hihi');
+  next();
+})
+
+app.get('/hi', (req:Request, res: Response) => {
+  console.log('hi')
+  res.send('hi')
+})
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
